db382acd15048330ba3e5b73057a3a236ecf6118
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-10-23 01:10:18 +0600

    MongoDB Connected

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 //Imports
 const express = require('express');
+const { MongoClient } = require('mongodb');
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+//Middleware use
 app.use(cors());
+app.use(express.json());
+
+//shadmansaalim
+//A5nF0672qqvJi2Iq
+
+//MONGO DB
+const uri = "mongodb+srv://shadmansaalim:A5nF0672qqvJi2Iq@cluster0.up7gk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("courseApp");
+        const usersCollection = database.collection("users");
+        // create a document to insert
+        const doc = {
+            title: "TEST",
+            content: "TEST",
+        }
+        const result = await usersCollection.insertOne(doc);
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     console.log('Hitting backend');
